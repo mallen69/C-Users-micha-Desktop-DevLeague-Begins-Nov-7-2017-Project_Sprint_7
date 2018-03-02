32f8a74e91ee937726505fa3572933d80ad246d1
Runjini <runjini_murthy@hotmail.com>
2018-03-01 14:23:36 -1000

    Updating for roadmap updates

--- Sprint03_Data_Operation/_jonhonda_dat/special_prj/SQLA_DB_facility_chars.py
+++ Sprint03_Data_Operation/_jonhonda_dat/special_prj/SQLA_DB_facility_chars.py
@@ -7,13 +7,9 @@ class Facility_Chars(Base):
     id = Column(Integer, primary_key=True)
     Fac_Name = Column(String(), unique=True)
     Permit_Table = Column(String()) #--permit table (1, 1A, 2, or none)
-<<<<<<< HEAD
-=======
     PBP_Category = Column(String()) #Priority based plan PBP_Category
-    NEL_Column = Column(String()) #nel column(s) assigned to this facility.
-                                    #*NOTE: purposely not associated w/ NEL_sample_Classes table b/c some facilities are assigned different wet and dry season columns
-                                    #which is represented in this table's NEL_Column field as xx & Yyy
->>>>>>> 95e8ddb91b9e507052f3593090bd907e5ebf658c
+    NEL_Column_Wet = Column(String()) #nel column(s) assigned to this facility. (wet season)
+    NEL_Column_Dry = Column(String()) #nel column(s) assigned to this facility. (dry season)
     existing_facility_risk_id = Column(Integer, ForeignKey('facility_risks.id')) #-- existing facility risk estimate record_id (modeled or from existing data sources)
     facility_monthly_rain_id = Column(Integer, ForeignKey('facility_monthly_rain.id')) #-- facility monthly rainfall records
     facility_type_id = Column(Integer, ForeignKey('facility_types.id'))## facility types as defined in the facility types table
@@ -21,9 +17,11 @@ class Facility_Chars(Base):
     Fac_Add = Column(String())
     Fac_TMK = Column(String())
     GW_Risk = Column(String())
+    Fac_Area_Acre = Column(Float)
     Fac_Area = Column(Float)
     Bldg_Area = Column(Float)
     Pave_Area = Column(Float)
+    Unpave_Area = Column(Float)
     Imperv = Column(Float)
     Tm = Column(Float)
     Runoff_Coeff = Column(Float)
@@ -54,24 +52,20 @@ class Facility_Chars(Base):
     Count_Chan = Column(Integer)
     Count_DS = Column(Integer)
     Count_TD = Column(Integer)
+    SMA_Area = Column(String())
     OFFSITE_SD_Exist = Column(String())
+    DS_SS_Exist = Column(String())
+    EM_Area = Column(Float)
     Dmg_Pave = Column(Integer)
     TFMR_Exist = Column(String())
-    DS_SS_Exist = Column(String())
     Can_Add_SD = Column(String())
-    Unpave_Area = Column(Float)
-    EM_Area = Column(Float)
-    Drainage_Area_Acres = Column(Float)
-<<<<<<< HEAD
-
-    # def __repr__(self):
-    #     return "<Locations(city='%s', country='%s', people_id='%s')>" % (
-    #                         self.city, self.country, self.people_id)
-=======
+    Indus_Area = Column(Float)
+    Indus_WQV = Column(Float)
+    Indus_WQFR = Column(Float)
     base_bmp_feasibility_test_results = relationship("Base_BMP_Feasibility_Test_Results") #setup 1:many relationship between table noted in this line, and this class
     combo_bmp_feasibility_test_results = relationship("Combo_BMP_Feasibility_Test_Results") #setup 1:many relationship between table noted in this line, and this class
+    existing_pollutant_concentrations = relationship("Existing_Pollutant_Concentrations") #setup 1:many relationship between table noted in this line, and this class
 
     def __repr__(self):
         return "<Facility_Chars(id='%s', Fac_Name ='%s')>" % (
                             self.id, self.Fac_Name)
->>>>>>> 95e8ddb91b9e507052f3593090bd907e5ebf658c
